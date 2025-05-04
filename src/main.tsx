83c202724c742dc173d5ea13e0acfc8e39ec03df
Hamza Ali <plomenet@gmail.com>
2025-05-04 19:15:02 +0800

    clean up

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import { createRoot } from 'react-dom/client'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import './theme/main.scss'
 
-import App from './App.tsx'
+import Index from './Index.tsx'
 
 import { BrowserRouter, Route, Routes } from 'react-router'
 
@@ -12,7 +12,7 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<App />} />
+        <Route path="/" element={<Index />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>,
